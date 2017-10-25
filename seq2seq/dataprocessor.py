9ca5e0f25543e8623526ce878eef590a63ab9ce5
mingzailao <mingzailao@163.com>
2017-10-25 20:04:24 +0800

    modify something #1

--- seq2seq/dataprocessor.py
+++ seq2seq/dataprocessor.py
@@ -10,49 +10,23 @@ from utils import tokenized,write_ob
 import torch.utils.data
 import jieba
 
-def extract_features(tokens):
-    "Given a list of token separate out words and features (if any)."
-    if not tokens:
-        return [], [], -1
-    split_tokens = [token.split(u"￨") for token in tokens]
-    split_tokens = [token for token in split_tokens if token[0]]
-    token_size = len(split_tokens[0])
-    assert all(len(token) == token_size for token in split_tokens), \
-        "all words must have the same number of features"
-    words_and_features = list(zip(*split_tokens))
-    words = words_and_features[0]
-    features = words_and_features[1:]
-    return words, features, token_size - 1
 
 class Dataset(torch.utils.data.Dataset):
     """
     # TODO: add docstring
     """
     # TODO: Construct function
-    def __init__(self,spath,tpath,opt):
+    def __init__(self,spath,tpath,opt=None):
         self.spath=spath
         self.tpath=tpath
-        source=tokenized(self.spath)
-        target=tokenized(self.tpath)
-        write_ob()
+        self.source=tokenized(self.spath)
+        self.target=tokenized(self.tpath)
+        # TODO maybe save the file
+        #
+        #
 
 
-    def _read_corpus_file(self,path,truncate,segmentation=False):
-        """
-        path : location of the source or target file
-        truncate : maximum sequence_length
-        returns: (word, features, nfeat) triples for each line
-        """
-        if segmentation:
-            with codecs.open(path,"r","utf-8") as corpus_file:
-                lines=(jieba.cut(line,cut_all=True) for line in corpus_file)
-        else:
-            with codecs.open(path,"r","utf-8") as corpus_file:
-                lines=(line.split() for line in corpus_file)
-                if truncate:
-                    lines=(line[:truncate] for line in lines)
-                for line in lines:
-                    yield extract_features(line)
+
 
     # TODO: get item function
     def __getitem__(self,i):
@@ -68,10 +42,16 @@ class Dataset(torch.utils.data.Dataset):
     # TODO: get attr
     def __getattr__(self,attr):
         pass
+    # TODO: get_batch
+    def get_batch(self,batch_size):
+        pass
+
 
 
 
 
 if __name__ == '__main__':
-    dataset=Dataset()
-    print dataset._read_corpus_file(os.path.abspath(os.path.join(data_path,"train.en")))
+    dataset=Dataset(spath=os.path.abspath(os.path.join(data_path,"train.en")),
+                    tpath=os.path.abspath(os.path.join(data_path,"train.zh")))
+    print dataset.source
+
