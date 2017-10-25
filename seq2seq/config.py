edcd4b1947caa6d6518d171d568d0881da5c0f8d
mingzailao <mingzailao@163.com>
2017-10-25 14:14:03 +0800

    add config and dataprocessor #1

---
+++ seq2seq/config.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import os.path
+
+
+root_path=os.path.abspath(os.path.join(os.path.split(os.path.realpath(__file__))[0],"../"))
+data_path=os.path.abspath(os.path.join(root_path,"data"))
+
+
+
+
+if __name__ == '__main__':
+    print root_path
+    print data_path
---
+++ seq2seq/dataprocessor.py
@@ -0,0 +1,32 @@
+import io
+import os
+
+import torch.utils.data
+
+class Dataset(torch.utils.data.Dataset):
+    """
+    # TODO: add docstring
+    """
+    # TODO: Construct function
+    def __init__(self):
+        "docstring"
+        pass
+    # TODO: get item function
+    def __getitem__(self,i):
+        pass
+
+    # TODO: get length of the dataset
+    def __len__(self):
+        pass
+
+    # TODO: iterator
+    def __iter__(self):
+        pass
+    # TODO: get attr
+    def __getattr__(self,attr):
+        pass
+
+
+
+
+
