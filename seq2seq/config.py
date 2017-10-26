eb3ce9b93acef159d78dc2cd4744dcf0ddbdb587
mingzailao <mingzailao@163.com>
2017-10-26 11:31:23 +0800

    fix #1

--- seq2seq/config.py
+++ seq2seq/config.py
@@ -6,6 +6,7 @@ import os.path
 root_path=os.path.abspath(os.path.join(os.path.split(os.path.realpath(__file__))[0],"../"))
 data_path=os.path.abspath(os.path.join(root_path,"data"))
 
+USE_CUDA=True
 
 
 
