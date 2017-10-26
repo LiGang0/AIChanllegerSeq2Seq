c81142c382dacc6a7550a7ded1444c73214cc280
mingzailao <mingzailao@163.com>
2017-10-26 10:22:35 +0800

    fix #1

--- seq2seq/dataprocessor.py
+++ seq2seq/dataprocessor.py
@@ -90,5 +90,5 @@ class Dataset(torch.utils.data.Dataset):
 if __name__ == '__main__':
     dataset=Dataset(spath=os.path.abspath(os.path.join(data_path,"train.en")),
                     tpath=os.path.abspath(os.path.join(data_path,"train.zh")))
-    print dataset.source[0]
+
 
