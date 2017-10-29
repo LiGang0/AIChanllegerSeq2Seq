ba6baf5e3552789502983105044cab165ca92b26
mingzailao <mingzailao@163.com>
2017-10-29 16:11:14 +0800

    no message

--- seq2seq/main.py
+++ seq2seq/main.py
@@ -1,8 +1,27 @@
 
-from config import DemoConfig
-from model.model1 import EncoderRNN,AttnDecoderRNN
+from config import DemoConfig,ReleaseConfig
+from model.model1 import EncoderRNN,AttnDecoderRNN,Train
 from dataprocessor import Dataset
 
+import argparse
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m","--mode",help= "Chose a mode")
+    args=parser.parse_args()
+
+    if args.mode=='release':
+        config=ReleaseConfig
+    elif args.mode=='demo':
+        config=DemoConfig
+    experiment=Train(config=config)
+    experiment.train()
+
+
+
 if __name__ == '__main__':
-    dataset=Dataset(config=DemoConfig)
-    encoder=EncoderRNN(input_size=dataset.source.n_words,hidden_size=DemoConfig.hidden_size)
+    main()
